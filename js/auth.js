fbec442f714be9bf8366842996dd83bbdcbe6b30
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-08-27 10:30:05 +0530

    implemnted sign up and signin

---
+++ js/auth.js
@@ -0,0 +1,67 @@
+$(document).ready(function() {
+    $('#signup').on('click', function(event) {
+        event.preventDefault();
+
+
+        const username = $('#username').val();
+        const email = $('#email').val();
+        const password = $('#password').val();
+        const confirmPassword = $('#confirm-password').val();
+        if(username === '' || email === '' || password === '' || confirmPassword === '') {
+            alert('Please fill out all fields.');
+            return;
+        }
+        const signUpData = {
+            username: username,
+            email: email,
+            password: password
+        };
+
+        $.ajax({
+            url: 'http://localhost:8085/api/v1/auth/register',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(signUpData),
+            success: function(result) {
+                localStorage.setItem("token",result.data.token);
+                alert('Sign up successful!');
+                window.location.href = 'signin.html';
+            },
+            error: function(error) {
+                console.error('Sign up failed:', error);
+                alert('Sign up failed. Please try again.');
+            }
+        });
+    });
+
+
+    $('#signin').on('click', function(event) {
+        event.preventDefault();
+
+        const email = $('#email').val();
+        const password = $('#password').val();
+        if (email === '' || password === '') {
+            alert('Please fill out both fields.');
+            return;
+        }
+
+        const signInData = {
+            email: email,
+            password: password
+        };
+        $.ajax({
+            url: 'http://localhost:8085/api/v1/auth/authenticate',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(signInData),
+            success: function(response) {
+                alert('Sign in successful!');
+                window.location.href = 'dashboard.html';
+            },
+            error: function(error) {
+                console.error('Sign in failed:', error);
+                alert('Sign in failed. Please check your credentials and try again.');
+            }
+        });
+    });
+});
